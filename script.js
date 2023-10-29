a5e408b476b01c6b56a3a395806a483cfe4ce781
Shanmathi <122082615+ShanmathiPrakash@users.noreply.github.com>
2023-10-29 22:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-<h1>Hello World</h1>
-    <h2>Introducing the Accio Ipsum</h2>
-    <p>Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.</p>
 
 
 //your code here
